61704da0be5200bddeaa5620a0a890fa47cc05c5
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-02-11 01:29:09 +0900

    HomeViewControllerの作成

--- SlideMenu/ViewControllers/HomeViewController.swift
+++ SlideMenu/ViewControllers/HomeViewController.swift
@@ -1,12 +1,18 @@
+//
+//  HomeViewController.swift
+//  SlideMenu
+//
+//  Created by 大野颯太 on 2022/02/11.
+//
+
 import UIKit
 
-class MenuViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
 }
