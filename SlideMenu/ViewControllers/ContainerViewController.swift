b86c14edef2b64951cb2489bfa091672ced5ee54
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-02-11 01:27:00 +0900

    MenuViewControllerの作成

--- SlideMenu/ViewControllers/ContainerViewController.swift
+++ SlideMenu/ViewControllers/ContainerViewController.swift
@@ -4,7 +4,7 @@ class ContainerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
